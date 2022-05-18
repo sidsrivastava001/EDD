611e989a97e29351c7befde3ada50fabe8adca94
sidsrivastava001 <sidsrivastava001@gmail.com>
2022-05-17 20:59:10 -0400

    update arduino code

--- dip_coater_EDD/dip_coater_EDD.ino
+++ dip_coater_EDD/dip_coater_EDD.ino
@@ -1,48 +1,87 @@
 #include <Stepper.h>
 
-const int steps = 200;  // change this to fit the number of steps for your motor
+#define BUTTON 2
+
+const float mmpersteps = -0.04;  // change this to fit the number of steps for your motor
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(steps, 8, 9, 10, 11);
+Stepper stepper(200, 8, 9, 10, 11);
 
-int previous;
 int motorSpeed;
 int cycles;
 int moveDist;
+int current;
 
 void setup() {
   // nothing to do inside the setup
-  previous = 0; // the previous reading from the analog input
+  current = 0;
   motorSpeed = 0;
   cycles = 0;
   moveDist = 0;
+  pinMode(BUTTON, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
-int setSpeedMotor() {
+void waitForButton() {
+   while(digitalRead(2)) {
+    delay(10);
+   }
+   while(!digitalRead(2)) {
+       delay(10);
+   }
+   Serial.println("BUTTON PRESSED");
+}
+
+void setSpeedMotor() {
   int sensorReading = analogRead(A0);
   // map it to a range from 0 to 100:
-  motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+  motorSpeed = map(sensorReading, 0, 1023, 0, 250);
   // set the motor speed:
-  myStepper.setSpeed(motorSpeed);
+  stepper.setSpeed(motorSpeed);
+  Serial.print("Speed: ");
+  Serial.println(motorSpeed);
   // step 1/100 of a revolution:
-  return motorSpeed
 }
 
-int goToPosition(int previous){
-  int val = analogRead(0);
-  moveDist = val-previous;
-  stepper.step(moveDist);
-  previous = val;
-  return previous;
+void goToPosition() {
+  int pos = analogRead(A0);
+  pos = map(pos, 0, 1023, 0, 100);
+  int moveDist = current-pos;
+  Serial.println(moveDist);
+  Serial.print("steppin: ");
+  Serial.println((int)(-moveDist/mmpersteps));
+  delay(100);
+  if(moveDist<0) {
+    
+    stepper.step((int)(moveDist/mmpersteps));
+    current = current + pos;
+    delay(2000);
+    stepper.step((int)(-moveDist/mmpersteps));
+    current = current - pos;
+  }
+  else {
+    stepper.step((int)(-moveDist/mmpersteps));
+    current = current - pos;
+    delay(2000);
+    stepper.step((int)(moveDist/mmpersteps));
+    current = current + pos;
+  }
+  
+
 }
 
 int setCycles(int cycles) {
   cycles = analogRead(1);
-  return cycles
+  return cycles;
 }
 
-void loop(int moveDist) {
-  motorSpeed = setSpeedMotor();
+void loop() {
+  waitForButton();
+  setSpeedMotor();
+  waitForButton();
+  delay(10);
+  goToPosition();
+  /*
   cycles = setCycles();
   for (int x = 0; x < cycles; x++) {
     goToPosition();
@@ -50,3 +89,5 @@ void loop(int moveDist) {
     stepper.step(-moveDist);
     delay(5000);
   }
+  */
+}
